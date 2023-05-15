bf476497f7219a4b61156aa20cca40d2d57b8ac7
L-Pircy <taveesha.agarwal@gmail.com>
2023-05-15 23:12:25 +0530

    mapping

--- contract.sol
+++ contract.sol
@@ -1,5 +1,7 @@
+pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
+
 
     event NewZombie(uint zombieId, string name, uint dna);
 
@@ -13,17 +15,21 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function _createZombie(string _name, uint _dna) private {
+    // declare mappings here
+    mapping (uint => address) public zombieBalance;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         emit NewZombie(id, _name, _dna);
     }
 
-    function _generateRandomDna(string _str) private view returns (uint) {
+    function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % dnaModulus;
     }
 
-    function createRandomZombie(string _name) public {
+    function createRandomZombie(string memory _name) public {
         uint randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
