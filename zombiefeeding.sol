9ac9560087297c9563df13504b7d83989e20e768
L-Pircy <taveesha.agarwal@gmail.com>
2023-05-18 21:25:46 +0530

    adding specific species genes at end

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -19,17 +19,20 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory{
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     KittyInterface kittyContract = KittyInterface(ckAddress);
-    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
         _targetDna = _targetDna % dnaModulus;
         uint newDna = (myZombie.dna+_targetDna)/2;
         _createZombie("NoName", newDna);
+        if (keccak256(abi.encodePacked(_species))==keccak256(abi.encodePacked("kitty"))){
+          newDna = newDna - newDna%100+99;
+      }
     }
 
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
-    feedAndMultiply(_zombieId, kittyDna);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 }
