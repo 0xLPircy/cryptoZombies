34ba06b9a38173c45fee7b455a455270d3b2f562
L-Pircy <taveesha.agarwal@gmail.com>
2023-05-27 14:32:14 +0530

    module4, ch11 Zombie loss, added else if we loose with cooldown

--- SolidityBegintoInterSmartContracts/zombieattack.sol
+++ SolidityBegintoInterSmartContracts/zombieattack.sol
@@ -19,5 +19,10 @@ contract ZombieAttack is ZombieHelper{
       enemyZombie.lossCount++;
       feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
     }
+        else{
+      myZombie.lossCount++;
+      enemyZombie.winCount++;
+      _triggerCooldown(myZombie);
+    }
   }
 }
